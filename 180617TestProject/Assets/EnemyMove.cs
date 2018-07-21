57fb3854715961b9b0a908e4ef2de68e53afb6a9
KR.Yoon <kyungryeol@yungyeonglyeol-ui-MacBook-Pro.local>
2018-07-21 13:21:39 +0900

    first commit

---
+++ 180617TestProject/Assets/EnemyMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour {
+
+GameObject player;
+
+Animator animator;
+NavMeshAgent nav;
+	// Use this for initialization
+
+	void Awake(){
+		player = GameObject.Find("Player");
+		animator = GetComponent<Animator>();
+		nav = GetComponent<NavMeshAgent>();
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination(player.transform.position);
+	}
+}
---
+++ 180617TestProject/Assets/EnymyMove2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnymyMove2 : MonoBehaviour {
+
+public Transform target;
+public Vector3 direction;
+public float velocity;
+public float accelaration;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveToTarget();
+	}
+
+	public void MoveToTarget(){
+		target = GameObject.Find("Player").transform;
+		direction = (target.position - transform.position).normalized;
+		accelaration = 0.1f;
+		velocity = (velocity+accelaration*Time.deltaTime);
+		float distance = Vector3.Distance(target.position, transform.position);
+		if(distance <= 10.0f){
+			this.transform.position = new Vector3 (transform.position.x + (direction.x*velocity),
+													transform.position.y + (direction.y*velocity),
+													transform.position.z);
+		}
+		else{
+			velocity = 0.0f;
+		}
+	}
+}
